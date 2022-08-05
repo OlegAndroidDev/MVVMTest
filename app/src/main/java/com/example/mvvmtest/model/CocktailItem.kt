0e21d1d78a5db39ac10048a7b6e31a88602df592
OlegAndroidDev <100316666+OlegAndroidDev@users.noreply.github.com>
2022-08-05 08:50:23 -0400

    First commit

---
+++ app/src/main/java/com/example/mvvmtest/model/CocktailItem.kt
@@ -0,0 +1,13 @@
+package com.example.mvvmtest.model
+
+data class CocktailItem(
+    val idDrink: String,
+    val strDrink: String,
+    val strTags: String,
+    val strImageSource: String,
+    val strIngredients1: String,
+    val strIngredients2: String,
+    val strIngredients3: String,
+    val strIngredients4: String,
+    val strIngredients5: String
+)
